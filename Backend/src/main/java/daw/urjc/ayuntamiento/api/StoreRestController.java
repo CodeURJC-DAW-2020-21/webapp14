2462cc4c8b92e98e11f885a1201731d2ebee3414
Adrián Muñoz <adrian.in2448@gmail.com>
2021-04-05 13:34:48 +0200

    APIs REST Controllers initialized

---
+++ Backend/src/main/java/daw/urjc/ayuntamiento/api/StoreRestController.java
@@ -0,0 +1,10 @@
+package daw.urjc.ayuntamiento.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/store")
+public class StoreRestController {
+
+}
