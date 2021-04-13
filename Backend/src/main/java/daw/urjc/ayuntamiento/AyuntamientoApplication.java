96ef004e39c065a48ab62e3a59d883ab80898d3b
Antonio Cuadrado <antoniocuad15@gmail.com>
2021-04-13 18:25:14 +0200

    Store Rest Controller updated
    The store REST controller has been updated with a beta version that allows to upload images

--- Backend/src/main/java/daw/urjc/ayuntamiento/AyuntamientoApplication.java
+++ Backend/src/main/java/daw/urjc/ayuntamiento/AyuntamientoApplication.java
@@ -1,13 +1,19 @@
 package daw.urjc.ayuntamiento;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AyuntamientoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AyuntamientoApplication.class, args);
+    }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
--- Backend/src/main/java/daw/urjc/ayuntamiento/api/StoreRestController.java
+++ Backend/src/main/java/daw/urjc/ayuntamiento/api/StoreRestController.java
@@ -1,13 +1,20 @@
 package daw.urjc.ayuntamiento.api;
 
+import daw.urjc.ayuntamiento.modules.Comment;
 import daw.urjc.ayuntamiento.modules.Store;
 import daw.urjc.ayuntamiento.service.LocalService;
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import java.net.URI;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.Optional;
 
 import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
@@ -15,6 +22,7 @@ import static org.springframework.web.servlet.support.ServletUriComponentsBuilde
 @RestController
 @RequestMapping("/api/store")
 public class StoreRestController {
+    private static final String POSTS_FOLDER = "service";
     @Autowired
     private LocalService service;
 
@@ -23,7 +31,7 @@ public class StoreRestController {
        return service.findAll();
     }
 
-    @GetMapping("/{id}")
+   @GetMapping("/{id}")
     public ResponseEntity<Store> getStore(@PathVariable long id) {
         Optional<Store> store = service.findId(id);
         if (store.isPresent()) {
@@ -31,13 +39,6 @@ public class StoreRestController {
         } else {
             return ResponseEntity.notFound().build();
         }
-    }
-
-    @PostMapping("/")
-    public ResponseEntity<Store> createStore(@RequestBody Store store) {
-        service.save(store);
-        URI location = fromCurrentRequest().path("/{id}").buildAndExpand(store.getId()).toUri();
-        return ResponseEntity.created(location).body(store);
     }
 
     @DeleteMapping("/{id}")
@@ -63,4 +64,29 @@ public class StoreRestController {
         }
     }
 
+    @GetMapping("/{id}/image")
+    public ResponseEntity<Object> getImage(@PathVariable long id) throws SQLException {
+
+        Optional<Store> store = service.findId(id);
+
+        if (store.isPresent()) {
+            int profilePhotoLength = (int) store.get().getImageField1().length();
+            return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/jpeg").body(new ByteArrayResource(store.get().getImageField1().getBytes(1, profilePhotoLength)));
+        } else
+            return ResponseEntity.notFound().build();
+    }
+
+
+    @PostMapping("/")
+    public ResponseEntity<Store> createStore(@ModelAttribute StoreDTO storeDTO) throws IOException, SQLException {
+        Store store = new Store(storeDTO.getName(),storeDTO.getDescription(),storeDTO.getFrontdescription(),storeDTO.getServices(),storeDTO.getOpenDay(),storeDTO.getCloseDay(),storeDTO.getOpenHour(),storeDTO.getCloseHour(),storeDTO.getStreet(),storeDTO.getLatitude(),storeDTO.getLength());
+        store.setComment(new LinkedList<Comment>());
+        MultipartFile img1 = storeDTO.getImageField1();
+        MultipartFile img2 = storeDTO.getImageField2();
+        store.setImageField1(BlobProxy.generateProxy(img1.getInputStream(), img1.getSize()));
+        service.save(store);
+        URI location = fromCurrentRequest().path("/{id}").buildAndExpand(store.getId()).toUri();
+
+        return ResponseEntity.created(location).body(store);
+    }
 }
