f25a4eef11e1808c2092ae843e8de3919bbe4157
Yeray <y.cornejo.2018@alumnos.urjc.es>
2021-04-26 13:02:02 +0200

    Try few changes

--- Frontend-Angular/src/app/app.module.ts
+++ Frontend-Angular/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { IndexComponent } from './index/index.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
--- Frontend-Angular/src/app/index/index.component.ts
+++ Frontend-Angular/src/app/index/index.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
 })
-export class AppComponent {
+export class IndexComponent {
   title = 'Frontend-Angular';
 }
