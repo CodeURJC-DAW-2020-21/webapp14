dafe9126e960762bf5adcf4e8a790901970b246e
Adrián Muñoz <adrian.in2448@gmail.com>
2021-04-27 19:45:41 +0200

    Main Event link fixed an other bug fixes

--- Frontend-Angular/src/app/app-routing.module.ts
+++ Frontend-Angular/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
-import {EventComponent} from './components/Events/events.component';
+import {EventsComponent} from './components/Events/events.component';
 import {MainEventComponent} from './components/MainEvent/mainevent.component';
 import {StoreFormComponent} from './components/StoreForm/storeform.component';
 
 const routes: Routes = [
   {path: '', component: IndexComponent},
-  {path: 'events', component: EventComponent},
+  {path: 'events', component: EventsComponent},
   {path: 'mainevent', component: MainEventComponent},
   {path: 'storeform', component: StoreFormComponent}
 ];
