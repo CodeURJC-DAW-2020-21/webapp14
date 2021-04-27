abcae3a024779b7d22dc81db387f152f4e255b8e
Yeray <y.cornejo.2018@alumnos.urjc.es>
2021-04-27 18:59:37 +0200

    Creation of main store

--- Frontend-Angular/src/app/components/Store/store.component.ts
+++ Frontend-Angular/src/app/components/Store/store.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'store',
+  templateUrl: './store.component.html',
+  styleUrls: ['./store.component.css']
 })
-export class AppComponent {
+export class StoreComponent {
   title = 'Frontend-Angular';
 }
