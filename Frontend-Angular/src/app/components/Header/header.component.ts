b1b29189dce9987d53a63ad61403483dda896208
Yeray <y.cornejo.2018@alumnos.urjc.es>
2021-04-27 11:20:21 +0200

    Fix error with header and animated effect

--- Frontend-Angular/src/app/components/Header/header.component.ts
+++ Frontend-Angular/src/app/components/Header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'Header',
+  selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
