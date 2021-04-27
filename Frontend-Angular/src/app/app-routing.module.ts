660714ec62501ae70fd09a2679799f6831fdc230
Alvaro <alvarocg26100@gmail.com>
2021-04-27 18:40:43 +0200

    Government page created in angular

--- Frontend-Angular/src/app/app-routing.module.ts
+++ Frontend-Angular/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 import {EventComponent} from './components/Events/events.component';
+import { GovernComponent } from './components/Government/government.component';
 
 const routes: Routes = [{path:'',component:IndexComponent},
-{path:'events',component:EventComponent},];
+{path:'events',component:EventComponent},
+{path:'govern',component:GovernComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
