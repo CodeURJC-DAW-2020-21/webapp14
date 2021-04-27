fd10f0fe1b6f63fad1ebb35ad9c0c3ded1a9e1c0
Alvaro <alvarocg26100@gmail.com>
2021-04-27 19:44:40 +0200

    minor fixes

--- Frontend-Angular/src/app/app-routing.module.ts
+++ Frontend-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
-import {EventComponent} from './components/Events/events.component';
+import {EventsComponent} from './components/Events/events.component';
 
 import { GovernComponent } from './components/Government/government.component';
 import { FormeventComponent} from './components/FormEvents/formevent.component';
@@ -9,7 +9,7 @@ import {MainEventComponent} from './components/MainEvent/mainevent.component';
 import {StoreFormComponent} from './components/StoreForm/storeform.component';
 
 const routes: Routes = [{path:'',component:IndexComponent},
-{path:'events',component:EventComponent},
+{path:'events',component:EventsComponent},
 {path:'govern',component:GovernComponent},
 {path:'formevent',component:FormeventComponent},
 {path: 'mainevent', component: MainEventComponent},
