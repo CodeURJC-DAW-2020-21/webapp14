f5e3e398807af2a5db0b7ba499e955a112e36de0
Yeray <y.cornejo.2018@alumnos.urjc.es>
2021-04-27 20:47:37 +0200

    Creation of main store

--- Frontend-Angular/src/app/app-routing.module.ts
+++ Frontend-Angular/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import {EventsComponent} from './components/Events/events.component';
 import { GovernComponent } from './components/Government/government.component';
 import { FormeventComponent} from './components/FormEvents/formevent.component';
 import {MainEventComponent} from './components/MainEvent/mainevent.component';
+import {MainStoreComponent} from './components/MainStore/mainstore.component';
 import {StoreFormComponent} from './components/StoreForm/storeform.component';
 import {UsersComponent} from './components/User/user.component';
 
@@ -20,7 +21,8 @@ const routes: Routes = [{path:'',component:IndexComponent},
 {path: 'storeform', component: StoreFormComponent},
 {path:'stores',component:StoreComponent},
 {path:'login',component:LoginComponent},
-{path:'user',component:UsersComponent}
+{path:'user',component:UsersComponent},
+{path:'mainstore',component:MainStoreComponent}
 ];
 
 
