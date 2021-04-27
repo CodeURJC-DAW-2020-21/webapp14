5907e42a1a2926a45fa50d62ec5dd49d199fbd6d
Adrián Muñoz <adrian.in2448@gmail.com>
2021-04-27 19:00:21 +0200

    Main Event template page created in Angular

--- Frontend-Angular/src/app/app-routing.module.ts
+++ Frontend-Angular/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 import {EventComponent} from './components/Events/events.component';
+import {MainEventComponent} from './components/MainEvent/mainevent.component';
 
 const routes: Routes = [{path:'',component:IndexComponent},
-{path:'events',component:EventComponent},];
+{path:'events',component:EventComponent}, {path:'mainevent',component:MainEventComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend-Angular/src/app/app.module.ts
+++ Frontend-Angular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IndexComponent } from './components/index/index.component';
 import { HeaderComponent } from './components/Header/header.component';
 import { FooterComponent } from './components/Footer/footer.component';
+import { MainEventComponent } from './components/MainEvent/mainevent.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent,
     IndexComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainEventComponent
   ],
   imports: [
     BrowserModule,
