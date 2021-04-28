e117a3a0bcb6a693154e38e8b19491775fbc2058
Israelpzl <israelpozuelo00@gmail.com>
2021-04-28 20:26:13 +0200

    add models

---
+++ Frontend-Angular/src/app/model/comment.model.ts
@@ -0,0 +1,11 @@
+
+
+export interface comment{
+
+  id:number;
+  name:string;
+  text:string;
+  date:Date;
+  image:string;
+
+}
---
+++ Frontend-Angular/src/app/model/event.model.ts
@@ -0,0 +1,20 @@
+
+
+
+export interface Event {
+	id: number;
+	activities: string;
+	name: string;
+	description: string;
+  place:string;
+	reward: string;
+  people:string;
+  price:string;
+  tag:string;
+  image:string;
+
+
+	//comments?: Comments[];
+
+
+}
