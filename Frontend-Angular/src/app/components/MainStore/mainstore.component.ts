e70d7c4b8cc323aeb831da530f7b8d55d6742979
Adrián Muñoz <adrian.in2448@gmail.com>
2021-04-30 13:00:09 +0200

    MainStore fixed

--- Frontend-Angular/src/app/components/MainStore/mainstore.component.ts
+++ Frontend-Angular/src/app/components/MainStore/mainstore.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { StoreService} from '../../service/store.service';
+import {ActivatedRoute} from '@angular/router';
+import {Local} from '../../model/local.model';
+import {main} from '@angular/compiler-cli/src/main';
 
 @Component({
   selector: 'mainstore',
   templateUrl: './mainstore.component.html',
   styleUrls: ['./mainstore.component.css']
 })
-export class MainStoreComponent {
+export class MainStoreComponent implements OnInit{
   title = 'Frontend-Angular';
+  id: number;
+  store: Local;
+  constructor(public storeService: StoreService, private activatedRoute: ActivatedRoute) {
+    let id = activatedRoute.snapshot.params['id'];
+    this.id = id;
+  }
+
+  ngOnInit() {
+    this.storeService.getStore(this.id).subscribe(
+      mainstore => {
+        this.store = mainstore;
+        console.log(this.store);
+      },
+      error => console.log("error")
+    );
+  }
 }
