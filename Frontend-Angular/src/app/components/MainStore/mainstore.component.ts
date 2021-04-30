0acb797b8c2bf2dc8ab8ae9e5369a247da2c0756
Israelpzl <israelpozuelo00@gmail.com>
2021-04-30 13:00:44 +0200

    fixed local error

--- Frontend-Angular/src/app/components/MainStore/mainstore.component.ts
+++ Frontend-Angular/src/app/components/MainStore/mainstore.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, ElementRef} from '@angular/core';
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+
+import { OSM } from 'ol/source';
+import * as Proj from 'ol/proj';
+
+
+export const DEFAULT_HEIGHT = '500px';
+export const DEFAULT_WIDTH = '500px';
+
+export const DEFAULT_LAT = -34.603490361131385;
+export const DEFAULT_LON = -58.382037891217465;
 
 @Component({
   selector: 'mainstore',
   templateUrl: './mainstore.component.html',
   styleUrls: ['./mainstore.component.css']
 })
-export class MainStoreComponent {
+export class MainStoreComponent implements OnInit, AfterViewInit {
   title = 'Frontend-Angular';
+
+  @Input() lat: number =  -34.603490361131385;
+  @Input() lon: number = -58.382037891217465;
+  @Input() zoom: number;
+  @Input() width: string | number = DEFAULT_WIDTH;
+  @Input() height: string | number = DEFAULT_HEIGHT;
+
+  target: string = 'map-' + Math.random().toString(36).substring(2);
+  map: Map;
+
+  private mapEl: HTMLElement;
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.mapEl = this.elementRef.nativeElement.querySelector('#' + this.target);
+    this.setSize();
+
+    this.map = new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: Proj.fromLonLat([this.lon, this.lat]),
+        zoom: this.zoom
+      }),
+
+    });
+  }
+
+  private setSize() {
+    if (this.mapEl) {
+      const styles = this.mapEl.style;
+      styles.height =  DEFAULT_HEIGHT;
+      styles.width = DEFAULT_WIDTH;
+    }
+  }
+
+
+
+
+
+
+}
+
+const cssUnitsPattern = /([A-Za-z%]+)$/;
+
+function coerceCssPixelValue(value: any): string {
+  if (value == null) {
+    return '';
+  }
+
+  return cssUnitsPattern.test(value) ? value : `${value}px`;
 }
