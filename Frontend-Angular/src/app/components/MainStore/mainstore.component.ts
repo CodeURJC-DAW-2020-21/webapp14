cb7004f01a8e0ebf650e7e01e6f7034323addd50
Alvaro <alvarocg26100@gmail.com>
2021-05-01 14:54:01 +0200

    hot fix

--- Frontend-Angular/src/app/components/MainStore/mainstore.component.ts
+++ Frontend-Angular/src/app/components/MainStore/mainstore.component.ts
@@ -8,6 +8,8 @@ import {Comment} from '../../model/comment.model';
 import { CommentService } from '../../service/comment.service';
 import {main} from '@angular/compiler-cli/src/main';
 import { LoginService } from '../../service/login.service';
+import { UserService } from '../../service/user.service';
+import { Users } from 'src/app/model/user.model';
 
 
 @Component({
@@ -19,16 +21,17 @@ export class MainStoreComponent implements OnInit{
   title = 'Frontend-Angular';
   id: number;
   store: Local;
-
+  user:Users;
   comment : Comment;
   text:string;
   date:Date;
   new: boolean;
-  constructor(private router: Router,private activatedRoute: ActivatedRoute,public storeService: StoreService,public commentService: CommentService ,public loginService: LoginService ) {
+  constructor(private router: Router,private activatedRoute: ActivatedRoute,public storeService: StoreService,public commentService: CommentService ,public loginService: LoginService,public userService: UserService ) {
 
     let id = activatedRoute.snapshot.params['id'];
     this.id = id;
     this.comment = {name:' ',text: ' ',date:  null, image :' '};
+    this.user = loginService.currentUser();
   }
 
   ngOnInit() {
@@ -51,18 +54,25 @@ export class MainStoreComponent implements OnInit{
 
   newCommentStore(){
 
-   
+    console.log(this.user);
     this.commentService.addComment(this.comment).subscribe(
-      (comment:Comment)=> {console.log(comment)
-        this.store.comment.push(comment)
+      (comentario:Comment)=> {console.log(comentario)
+        this.store.comment.push(comentario)
         this.storeService.addStore(this.store).subscribe(
-          (tienda:Local) => console.log(tienda),
+          (tienda:Local) => {console.log(tienda)
+            this.user.comment.push(comentario);
+            this.user.commentPlaces.push(tienda.name);
+            this.userService.addUser(this.user).subscribe(
+              (usuario:Users) => console.log(usuario),
+               error => alert('Error al actualizar el usuario : ' + error)
+        ); 
+          },
           error => alert('Error al actualizar la tienda : ' + error)
-        );},
+        ); 
+      },
       error => alert('Error al crear el comentario: ' + error)
     );
-    
-
+  
   }
 
 }
