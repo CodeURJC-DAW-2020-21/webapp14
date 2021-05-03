f399e825eb9b01dde879d198f0d703ac6a27929a
Adrián Muñoz <adrian.in2448@gmail.com>
2021-05-03 14:16:33 +0200

    SPA package initialized

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapBoxToken: 'pk.eyJ1IjoidGhleWVjb2NhIiwiYSI6ImNrbzZ6cXRodjA2YzcydXA3Nzk1YTBjazkifQ.8zXsr2sBUwIe1PBDxP0zCA'
 };
